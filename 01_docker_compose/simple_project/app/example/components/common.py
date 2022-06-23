64f3237763ebb701db735fc8c774295e23823ba7
anastasiakuklina <anastasijakuklina@gmail.com>
2022-06-23 12:55:50 +0400

    combine together 01 and 02 tasks

--- 01_docker_compose/simple_project/app/example/components/common.py
+++
@@ -1,8 +0,0 @@
-import os
-from pathlib import Path
-
-BASE_DIR = Path(__file__).resolve().parent.parent
-
-SECRET_KEY = os.environ.get('SECRET_KEY')
-
-DEBUG = os.environ.get('DEBUG', False) == 'True'
--- 01_docker_compose/simple_project/app/example/components/urls.py
+++
@@ -1,7 +0,0 @@
-import os
-
-# ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split()
-ALLOWED_HOSTS = ['127.0.0.1']
-
-ROOT_URLCONF = 'example.urls'
-
